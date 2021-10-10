c5a93f6d469744cf9d312862f31a23f959512a34
The24thDS <davidsima@windowslive.com>
2021-10-10 13:05:14 +0300

    fix(setup): fixed yml updater not updated translated files correctly

--- yml-file-updater.js
+++ yml-file-updater.js
@@ -1,9 +1,10 @@
-const versionRegExp = /NE_VERSION: "New Enclaves v(?<version>.*)"/;
+const versionRegExp = /NE_VERSION: "(?<name>.*) v(?<version>.*)"/;
 
 module.exports.readVersion = (contents) => {
   return contents.match(versionRegExp).groups.version;
-}
+};
 
 module.exports.writeVersion = (contents, version) => {
-  return contents.replace(versionRegExp, `NE_VERSION: "New Enclaves v${version}"`);
-}
+  const name = contents.match(versionRegExp).groups.name;
+  return contents.replace(versionRegExp, `NE_VERSION: "${name} v${version}"`);
+};
